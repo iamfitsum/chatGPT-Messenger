26235a39542233ae63bc4a0d53fbf70a7bb000e9
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-03-22 04:32:39 +0300

    Added Firebase Firestore, Functionality to Add chats and remove chats by setting up realtime listner

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import NewChat from "./NewChat";
+import { db } from "@/firebase";
+import { collection, orderBy, query } from "firebase/firestore";
+import ChatRow from "./ChatRow";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats"),
+        orderBy("createdAt", "desc")
+      )
+  );
+
+  console.log(chats);
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -14,6 +28,9 @@ function SideBar() {
           <div>{/* TODO: Model Selection */}</div>
 
           {/* TODO: Map Through The Chat Rows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
 
